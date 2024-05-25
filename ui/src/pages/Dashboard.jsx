a4c643f3d7378de819a5976a6fd8977ae6a5b22f
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-05-25 18:57:05 +0200

    Inserted data from endpoint into tables and added pagination

--- ui/src/pages/Dashboard.jsx
+++ ui/src/pages/Dashboard.jsx
@@ -3,46 +3,45 @@ import axios from 'axios';
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import Nav from "../components/Nav";
 import "../styles/dashboard.css";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination, Box } from '@mui/material';
 
 export default function Dashboard(props) {
   const [chartData, setChartData] = useState([]);
-  const [tableData, setTableData] = useState([])
+  const [tableData, setTableData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(12);
 
-  const months = 480;
+  const months = 189;
+  const quantity = 10;
 
   useEffect(() => {
-    axios.get(`http://localhost:9000/api/bonds/${props.type}/${months}`)
+    axios.get(`http://localhost:9000/api/bonds/${props.type}/${quantity}/${months}`)
       .then(response => {
-        setTableData([]);
-        const newData = response.data.map((value, index) => ({
-          month: index + 1,  
-          value: Math.round(value * 100) / 100       
+        const newChartData = response.data.map((row, index) => ({
+          month: index + 1,
+          finalResult: Math.round(row[9] * 100) / 100,
         }));
-        setChartData(newData);
-        // const apiData = response.data.map(item => ({
-        //   col1: Math.round((item[0] || 0) * 100) / 100,
-        //   col2: Math.round((item[1] || 0) * 100) / 100,
-        //   col3: Math.round((item[2] || 0) * 100) / 100,
-        //   col4: Math.round((item[3] || 0) * 100) / 100,
-        //   col5: Math.round((item[4] || 0) * 100) / 100,
-        //   col6: Math.round((item[5] || 0) * 100) / 100,
-        //   col7: Math.round((item[6] || 0) * 10000) / 10000,
-        //   col8: Math.round((item[7] || 0) * 100) / 100,
-        //   col9: Math.round((item[8] || 0) * 100) / 100,
-        //   col10: Math.round((item[9] || 0) * 100) / 100,
-        //   col11: Math.round((item[10] || 0) * 100) / 100,
-        //   col12: Math.round((item[11] || 0) * 100) / 100
-        // }));
-        
-        // setTableData(prevData => [...prevData, ...apiData]);
+        setChartData(newChartData);
 
+        const newTableData = response.data.map((row, index) => ({
+          month: index + 1,
+          quantity: row[1],
+          buyPrice: Math.round(row[2] * 100) / 100,
+          basePrice: Math.round(row[3] * 100) / 100,
+          percentage: Math.round(row[4] * 100) / 100,
+          grossValue: Math.round(row[5] * 100) / 100,
+          penalty: Math.round(row[6] * 100) / 100,
+          withdrawal: Math.round(row[7] * 100) / 100,
+          account: Math.round(row[8] * 100) / 100,
+          finalResult: Math.round(row[9] * 100) / 100,
+        }));
 
+        setTableData(newTableData);
       })
       .catch(error => console.error('Error:', error));
-  }, [props.type]);  
+  }, [props.type]);
 
-  console.log(chartData)
+  console.log(chartData);
 
   const colorMap = {
     'OTS': 'rgb(30,185,128)',
@@ -56,65 +55,91 @@ export default function Dashboard(props) {
     'defaultColor': 'rgb(30,185,128)'
   };
 
+  const tableCellStyle = {
+    border: '1px solid white',
+    color: 'white'
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
     <>
-    <div className="dashboard--wrapper">
-      <Nav />
-      <div className="chart--wrapper">
-        <p className="chart--text">Chart for <span style={{ color: colorMap[props.type] || 'defaultColor' }}>{props.type}</span></p>
-        <div style={{ width: '70vw', height: '500px' }}>
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              data={chartData}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-            >
-              <XAxis dataKey="month" type="number" domain={[1, months]} allowDecimals={false} ticks={Array.from({length: months}, (_, i) => i + 1)} />
-              <YAxis domain={[10000, 'auto']} />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="value" stroke={colorMap[props.type] || 'defaultColor'} dot={{ r: 0 }} />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-        {/* <div className="table--wrapper">
+      <div className="dashboard--wrapper">
+        <Nav />
+        <div className="chart--wrapper">
+          <p className="chart--text">Chart for <span style={{ color: colorMap[props.type] || 'defaultColor' }}>{props.type}</span></p>
+          <div style={{ width: '70vw', height: '500px' }}>
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                data={chartData}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+              >
+                <XAxis dataKey="month" type="number" domain={[1, months]} allowDecimals={false} ticks={Array.from({ length: months }, (_, i) => i + 1)} />
+                <YAxis domain={[10000, 'auto']} />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="finalResult" stroke={colorMap[props.type] || 'defaultColor'} dot={{ r: 0 }} />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+          <div className="table--wrapper">
             <TableContainer component={Paper} sx={{
-              backgroundColor: 'transparent', 
-              color: 'white'
+              backgroundColor: 'transparent',
+              color: 'white',
+              boxShadow: 'none'
             }}>
-              <Table sx={{ width: '65vw', border: '1px solid white' }} aria-label="simple table">
+              <Table sx={{ width: '65vw', border: '1px solid white'}} aria-label="simple table">
                 <TableHead>
                   <TableRow>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>End of month</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Quantity</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Buy Price</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Base Price</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Percentage</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Current Value</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Penalty</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Withdrawal</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Account</TableCell>
-                    <TableCell sx={{ border: '1px solid white', color: 'white' }}>Final result</TableCell>
+                    <TableCell sx={tableCellStyle}>Month</TableCell>
+                    <TableCell sx={tableCellStyle}>Quantity</TableCell>
+                    <TableCell sx={tableCellStyle}>Buy Price</TableCell>
+                    <TableCell sx={tableCellStyle}>Base Price</TableCell>
+                    <TableCell sx={tableCellStyle}>Percentage</TableCell>
+                    <TableCell sx={tableCellStyle}>Gross Value</TableCell>
+                    <TableCell sx={tableCellStyle}>Penalty</TableCell>
+                    <TableCell sx={tableCellStyle}>Withdrawal</TableCell>
+                    <TableCell sx={tableCellStyle}>Account</TableCell>
+                    <TableCell sx={tableCellStyle}>Final Result</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {tableData.map((row, index) => (
+                  {tableData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
                     <TableRow key={index}>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col1}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col3}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col4}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col6}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col7}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col8}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col9}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col10}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col11}</TableCell>
-                      <TableCell sx={{ border: '1px solid white', color: 'white' }}>{row.col12}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.month}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.quantity}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.buyPrice}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.basePrice}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.percentage}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.grossValue}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.penalty}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.withdrawal}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.account}</TableCell>
+                      <TableCell sx={tableCellStyle}>{row.finalResult}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
               </Table>
+              <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2}}>
+                <TablePagination
+                  rowsPerPageOptions={[12]}
+                  component="div"
+                  count={tableData.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </Box>
             </TableContainer>
-        </div> */}
+          </div>
         </div>
       </div>
     </>
